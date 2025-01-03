a705e2a6dceb84cc8da7bc1fd254b265c04dce06
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2025-01-03 18:21:10 +0100

    feat: leaflet types

---
+++ spotwise/src/types/vue3-leaflet.d.ts
@@ -0,0 +1,6 @@
+declare module 'vue3-leaflet' {
+    import { DefineComponent } from 'vue';
+    export const LMap: DefineComponent<{}, {}, any>;
+    export const LTileLayer: DefineComponent<{}, {}, any>;
+    export const LMarker: DefineComponent<{}, {}, any>;
+}
